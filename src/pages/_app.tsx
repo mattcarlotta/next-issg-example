f5e3b16a6414bc5e865779c20e7c0a11f7c54070
Matt <carlotta.matt@gmail.com>
2020-10-07 19:41:47 -0700

    Remove emotion caching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
-import { CacheProvider } from "@emotion/core";
-import { cache } from "emotion";
 import GlobalStylesheet from "~styles/globalStylesheet";
 import "react-toastify/dist/ReactToastify.css";
 import { AppProps, FC } from "~types";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <CacheProvider value={cache}>
+  <>
     <Head>
       <meta
         name="viewport"
@@ -26,7 +24,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => (
       draggable
       pauseOnHover
     />
-  </CacheProvider>
+  </>
 );
 
 export default App;
